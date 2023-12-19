88f3f85e0a5f25c02f9eec646c60824e07c8ef51
Ardian F Firmansyah <ardianfirmansyah123@gmail.com>
2023-12-19 16:21:32 +0700

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,5 +21,6 @@ class HomeController extends Controller
     public function index()
     {
         $json = Modul::generateUrl('Ardian Ferdy Firmansyah', '3374102302990001', $this->auth(), 'https://api-satusehat.kemkes.go.id/kyc/v1/generate-url', 'production');
+        dd($json);
     }
 }
