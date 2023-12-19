12a1d95d00d9c734b467a89cd713d2535fd208cd
Ardian F Firmansyah <ardianfirmansyah123@gmail.com>
2023-12-19 09:29:14 +0700

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,15 +5,20 @@ namespace App\Http\Controllers;
 use Bridging;
 use Illuminate\Http\Request;
 use Master;
+use Modul;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function auth()
     {
         $key = json_decode((file_get_contents('http://' . $_SERVER['HTTP_HOST'] . '/satu-sehat/key')), true);
         $pusk = json_decode(Master::SecurityDecode($key['data']), true);
 
         $token = Bridging::GetTokenBaru($pusk['client'], $pusk['secret']);
         return $token;
+    }
+    public function index()
+    {
+        $json = Modul::generateUrl('Ardian Ferdy Firmansyah', '3374102302990001', $this->auth(), 'api_url = https://api-satusehat.kemkes.go.id/kyc/v1/generate-url', 'production');
     }
 }
