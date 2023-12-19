3dd4d21c539ad179cd27963711c66b82d11d83bd
Ardian F Firmansyah <ardianfirmansyah123@gmail.com>
2023-12-19 15:49:03 +0700

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,7 +11,8 @@ class HomeController extends Controller
 {
     public function auth()
     {
-        $key = json_decode((file_get_contents('http://' . $_SERVER['HTTP_HOST'] . '/satu-sehat/key')), true);
+        // $key = json_decode((file_get_contents('http://' . $_SERVER['HTTP_HOST'] . '/satu-sehat/key')), true);
+        $key = json_decode((file_get_contents('http://119.2.50.170:6512/satu-sehat/key')), true);
         $pusk = json_decode(Master::SecurityDecode($key['data']), true);
 
         $token = Bridging::GetTokenBaru($pusk['client'], $pusk['secret']);
